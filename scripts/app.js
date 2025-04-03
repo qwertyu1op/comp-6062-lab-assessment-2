a4350206fd5108d58204777e0730cb19e2c8dd12
nihal brarath <brarath.nihal@gmail.com>
2025-04-03 19:42:15 -0400

    Pressure Monitor Interface
    Change the wepage as the slider moves side to side, 0-100%

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+/// Create a new Vue app
+const app = Vue.createApp({
+    /// Define the data for the app
+    data() {
+        return {
+            myFullName: "Nihal Brarath",
+            pressureMonitorValue: 65,
+            pressureMonitorLevels: [  
+                { id: 'danger-low', color: 'danger', label: 'Danger - Low!', min: 0, max: 10 }, 
+                { id: 'warn-low', color: 'warning', label: 'Warning - Low', min: 11, max: 30 }, 
+                { id: 'safe', color: 'safe', label: 'Safe', min: 31, max: 70 },
+                { id: 'warn-high', color: 'warning', label: 'Warning - High', min: 71, max: 90 }, 
+                { id: 'danger-high', color: 'danger', label: 'Danger - High', min: 91, max: 100 } 
+                ] 
+
+        };
+    },
+    /// Define the methods for the app
+    methods: {
+        isActive(pressureLevel) {
+            /// Logic here for if the pressure is within the level parameters return boolean
+            if(this.pressureMonitorValue>=pressureLevel.min && this.pressureMonitorValue<=pressureLevel.max){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+});
+
+/// Mount the app to the #app element
+app.mount('#app');
